630f2a2458cd56bd9a88a4ba07fa61129d3f6bff
vdeq79 <xszh79@outlook.com>
2023-05-07 22:29:00 +0200

    Proyecta al modelo generado, existen errores

--- src/motionCap.py
+++ src/motionCap.py
@@ -13,7 +13,7 @@ else:
 
 cap = cv2.VideoCapture(0)
 detector = PoseDetector()
-tshirt = cv2.imread("../t-shirt.png")
+tshirt = cv2.imread("../lena.tif")
 
 if cap.isOpened():
     hframe = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -32,7 +32,7 @@ if cap.isOpened():
             framerectificado = cv2.undistort(framebgr, camara.cameraMatrix, camara.distCoeffs, None, matrix)
             framerecortado = framerectificado[roi_y : roi_y + roi_h, roi_x : roi_x + roi_w]
 
-            framerecortado = detector.findPose(framerecortado)
+            framerecortado = detector.findPose(framerecortado, draw=False)
             lmList, bboxInfo = detector.findPosition(framerecortado, draw=False)
 
             if bboxInfo:
@@ -49,8 +49,8 @@ if cap.isOpened():
                 diff1 = np.subtract(lmList[13][1:3],lmList[11][1:3])
                 diff2 = np.subtract(lmList[14][1:3],lmList[12][1:3])
 
-                p8 = np.array(p1 + 2/3*diff1+[8,-8]).astype(int)
-                p3 = np.array(p2 + 2/3*diff2+[-8,-8]).astype(int)
+                p8 = np.array(p1 + 2/3*diff1+[16,-8]).astype(int)
+                p3 = np.array(p2 + 2/3*diff2+[-16,-8]).astype(int)
 
                 #print(diff1[1])
 
@@ -61,12 +61,33 @@ if cap.isOpened():
                 p5 = np.array([1/2*(lmList[24][1]+p2[0]), lmList[24][2]]).astype(int)
                 
 
-                points = [p1,p2,p3,p4,p5,p6,p7,p8]
-                
-                for point in points:
-                    framerecortado = cv2.circle(framerecortado, point, radius=5, color=(255,0,0), thickness=-1)
+                #points = np.array([p1,p2,p3,p4,p5,p6,p7,p8], np.int32)
 
-                #framerecortado = augmentAruco(corner, None, framerecortado, tshirt)
+                points = np.array([p2,p1,p8,p7,p6,p5,p4,p3], np.int32)
+
+                #for point in points:
+                #    framerecortado = cv2.circle(framerecortado, point, radius=5, color=(255,0,0), thickness=-1)
+
+                src_h, src_w = tshirt.shape[:2]
+                #src_points = np.array([[0, 0], [src_w, 0], [src_w, src_h], [0, src_w]])
+                #dst_points = np.array([p2,p1,p6,p5],int) 
+
+
+                src_points = np.array([[1/5*src_w,0], [4/5*src_w,0], [src_w, 1/3*src_h], [4/5*src_w, 1/3*src_h], [4/5*src_w, src_h], [1/5*src_w, src_h], [1/5*src_w, 1/3*src_w], [0,1/3*src_w]],np.int32)
+                dst_points = points
+                #print(dst_points)
+
+                matrix2, _ = cv2.findHomography(srcPoints=src_points, dstPoints=dst_points)
+                warp_image = cv2.warpPerspective(tshirt, matrix2, (framerecortado.shape[1], framerecortado.shape[0]))
+
+                points = points.reshape(-1,1,2)
+                #cv2.fillPoly(framerecortado, [points], color=(255,255,255))
+
+
+                cv2.fillConvexPoly(framerecortado,dst_points, (0,0,0) )
+                #cv2.imshow("warp", warp_image)
+
+                framerecortado+=warp_image
 
 
             cv2.imshow("RECORTADO", framerecortado)
