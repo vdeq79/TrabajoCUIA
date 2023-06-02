8ada887216bd44bea3fc9819154c61f996a70d4d
vdeq79 <xszh79@outlook.com>
2023-06-02 18:41:10 +0200

    Extrae las funciones y elimina el modelo en casos extremos

--- src/functions.py
+++ src/functions.py
@@ -45,7 +45,65 @@ def drawArucoAxis(corner, id, img, cameraMatrix, distCoeffs, markerSize, axisSiz
     cv2.putText(img, f"Dist:{tVec[0][0][2]}", corner[0][0].astype(int), cv2.FONT_HERSHEY_PLAIN, 1.3 ,(0,0,255), 2, cv2.LINE_AA)
 
 
-def getCenter(corner, id, img, draw=True):
-    centro = np.sum(corner)/4
+#Dado el modelo escaneado de una persona, construir el modelo de la camiseta
+def getShirtModelPoints(lmList):
+    p1 = np.add(lmList[11][1:3], [0,-15]) 
+    p2 = np.add(lmList[12][1:3], [0,-15])
+
+    #Aquí calculamos si el usuario tiene el brazo levantado hallando la distancia entre el codo y el hombro
+    diff1 = np.subtract(lmList[13][1:3],lmList[11][1:3])
+    diff2 = np.subtract(lmList[14][1:3],lmList[12][1:3])
+
+    p8 = np.array(p1 + 2/3*diff1+[16,-8]).astype(int)
+    p3 = np.array(p2 + 2/3*diff2+[-16,-8]).astype(int)
+
+    #Para las esquinas P4 y P7, fijamos un mínimo de 30 en Y por si el codo y el hombre están muy juntos
+    p7 = np.add(p1, [0, np.absolute(3/4*diff1[1]) if np.absolute(diff1[1])>30 else 30 ]).astype(int)
+    p4 = np.add(p2, [0, np.absolute(3/4*diff2[1]) if np.absolute(diff2[1])>30 else 30 ]).astype(int)
+    
+    #Las esquinas inferiores son un poco menos anchas que las caderas del modelo humano
+    p6 = np.array([1/2*(lmList[23][1]+p1[0]), lmList[23][2]]).astype(int)
+    p5 = np.array([1/2*(lmList[24][1]+p2[0]), lmList[24][2]]).astype(int)
+    
+    #points puntos finales para la camiseta
+    points = np.array([p2,p1,p8,p7,p6,p5,p4,p3], np.int32)
+
+    return points
 
 
+def getImageInShirt(points, img, frame_shape):
+
+    #Creamos una nueva ventana con fondo negro
+    nueva_ventana = np.zeros(frame_shape, dtype=np.uint8)
+    tratado = False
+
+    #Determinar el nuevo tamaño de la imagen dado los puntos del modelo de camiseta
+    min_w = np.min(points[:,0])  
+    min_h = np.min(points[:,1])
+    max_w = np.max(points[:,0])  
+    max_h = np.max(points[:,1]) 
+
+    if 0<=min_w and min_w<frame_shape[1] and 0<=min_h and min_h<frame_shape[0]:
+        nuevo_w=max_w-min_w
+        nuevo_h=max_h-min_h
+        nuevo_dim = (nuevo_w, nuevo_h)
+
+        resized_img = cv2.resize(img, nuevo_dim, interpolation=cv2.INTER_AREA)
+        cv2.imshow("Resize",resized_img)
+
+        #Pintamos la camiseta de blanco
+        cv2.fillConvexPoly(nueva_ventana,points, (255,255,255) )
+
+        #Determinar el rectángulo que inscribe a la camiseta
+        rectangulo = nueva_ventana[min_h:np.min([max_h, nueva_ventana.shape[0]] ), min_w:np.min([max_w, nueva_ventana.shape[1]] )]
+
+        #Calculamos la región máxima que podemos hacer and, pues es posible que algún punto salga fuera de la pantalla
+        limited_img = resized_img[0:np.min([nueva_ventana.shape[0]-min_h, nuevo_h ]), 0:np.min([nueva_ventana.shape[1]-min_w, nuevo_w]) ]
+
+        #Hacer un and del rectángulo con la imagen escalada
+        nueva_ventana[min_h:np.min([max_h, nueva_ventana.shape[0]] ), min_w:np.min([max_w, nueva_ventana.shape[1]])]=cv2.bitwise_and(rectangulo, limited_img)
+        
+        cv2.imshow("mask", nueva_ventana)
+        tratado = True
+
+    return (tratado,nueva_ventana)
--- src/motionCap.py
+++ src/motionCap.py
@@ -13,7 +13,7 @@ else:
 
 cap = cv2.VideoCapture(0)
 detector = PoseDetector()
-original = cv2.imread("../lena.tif")
+original = cv2.imread("../zebra.jpg")
 
 if cap.isOpened():
     hframe = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -36,100 +36,15 @@ if cap.isOpened():
             lmList, bboxInfo = detector.findPosition(framerecortado, draw=False)
 
             if bboxInfo:
-                '''corner = []
-                for i in [11,12,24,23]:
-                    corner.append([lmList[i][1], lmList[i][2]])
 
-                corner = np.array(corner)'''
-                
-                
-                p1 = np.add(lmList[11][1:3], [0,-15]) 
-                p2 = np.add(lmList[12][1:3], [0,-15])
+                points = getShirtModelPoints(lmList)
+                tratado, nueva_ventana = getImageInShirt(points, original, framerecortado.shape)
 
-                diff1 = np.subtract(lmList[13][1:3],lmList[11][1:3])
-                diff2 = np.subtract(lmList[14][1:3],lmList[12][1:3])
-
-                p8 = np.array(p1 + 2/3*diff1+[16,-8]).astype(int)
-                p3 = np.array(p2 + 2/3*diff2+[-16,-8]).astype(int)
-
-                #print(diff1[1])
-
-                p7 = np.add(p1, [0, np.absolute(3/4*diff1[1]) if np.absolute(diff1[1])>30 else 30 ]).astype(int)
-                p4 = np.add(p2, [0, np.absolute(3/4*diff2[1]) if np.absolute(diff2[1])>30 else 30 ]).astype(int)
-                
-                p6 = np.array([1/2*(lmList[23][1]+p1[0]), lmList[23][2]]).astype(int)
-                p5 = np.array([1/2*(lmList[24][1]+p2[0]), lmList[24][2]]).astype(int)
-                
-                #points puntos finales para la camiseta
-                points = np.array([p2,p1,p8,p7,p6,p5,p4,p3], np.int32)
-
-                #for point in points:
-                #    framerecortado = cv2.circle(framerecortado, point, radius=5, color=(255,0,0), thickness=-1)
-
-                nuevo_w = p8[0]-p3[0]
-                nuevo_h = p5[1]-p2[1]
-                nuevo_dim = (nuevo_w, nuevo_h)
-
-
-                tshirt = cv2.resize(original, nuevo_dim, interpolation=cv2.INTER_AREA)
-
-                src_h, src_w = tshirt.shape[:2]
-
-                src_points = np.array([[1/5*src_w,0], [4/5*src_w,0], [src_w, 1/3*src_h], [4/5*src_w, 1/3*src_h], [4/5*src_w, src_h], [1/5*src_w, src_h], [1/5*src_w, 1/3*src_h], [0,1/3*src_h]],np.int32)
-
-
-                mask = np.zeros(tshirt.shape[:2], dtype=np.uint8)
-                cv2.drawContours(mask, [points], -1, (255,255,255), -1, cv2.LINE_AA)
-
-                res = cv2.bitwise_or(tshirt, tshirt, mask=mask)
-                cv2.imshow("Samed size black image", tshirt)
-                dst_points = points
-
-
-                #matrix2, _ = cv2.findHomography(srcPoints=src_points, dstPoints=dst_points)
-                #warp_image = cv2.warpPerspective(res, matrix2, (framerecortado.shape[1], framerecortado.shape[0]))
-
-
-                points = points.reshape(-1,1,2)
-
-                cv2.polylines(framerecortado, points, True, (255,0,0), 8, cv2.LINE_AA)
-                cv2.fillConvexPoly(framerecortado,dst_points, (0,0,0) )
-
-                #nueva_ventana = framerecortado.copy()
-                #nueva_ventana[nueva_ventana!=0] = 255
-                nueva_ventana = np.zeros(framerecortado.shape, dtype=np.uint8)
-                #print("1",p3[0],p8[0])
-                #print("2",p2[1],p5[1])
-                #print("3",p2[0],p5[0])
-                #print(nueva_ventana.shape)
-
-                #print([nueva_ventana.shape[1]-p2[1], nuevo_h ])
-                cv2.fillConvexPoly(nueva_ventana,dst_points, (255,255,255) )
-                rectangulo = nueva_ventana[p2[1]:np.min([p5[1], nueva_ventana.shape[0]] ), p3[0]:p8[0]]
-                auxiliar = rectangulo + tshirt[0:np.min([nueva_ventana.shape[0]-p2[1], nuevo_h ]),]
-
-                #usar and en lugar de sumar directamente
-                nueva_ventana[p2[1]:np.min([p5[1], nueva_ventana.shape[0]] ), p3[0]:p8[0]]=cv2.bitwise_and(rectangulo, tshirt[0:np.min([nueva_ventana.shape[0]-p2[1], nuevo_h ]),])
-
-                #nueva_ventana[p2[1]:np.min([p5[1], nueva_ventana.shape[0]] ), p3[0]:p8[0]] = [auxiliar[i] if np.any(rectangulo[i]==0) else rectangulo[i] for i in range(len(rectangulo))]
-
-                '''for i in range(rectangulo.shape[0]):
-                    for j in range(rectangulo.shape[1]):
-                        if np.all(rectangulo[i,j]==0):
-                            rectangulo[i,j]=auxiliar[i,j]'''
-                
-                
-
-                #mask += warp_image
-                cv2.imshow("mask", nueva_ventana)
-                
-
-                #cv2.imshow("warp", warp_image)
-                #framerecortado+=res
-
-                #framerecortado = cv2.bitwise_and(framerecortado, res )
-
-
+                if tratado:
+                    #Pintamos la camiseta de negro
+                    cv2.fillConvexPoly(framerecortado,points, (0,0,0) )
+                    #Juntamos ambas ventanas
+                    framerecortado = cv2.bitwise_or(framerecortado, nueva_ventana )
 
             cv2.imshow("RECORTADO", framerecortado)
             if cv2.waitKey(1) == ord(' '):
